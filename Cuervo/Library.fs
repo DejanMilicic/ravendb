57aa6e90d5e545fa8627bf1f0f3a4f004a2365eb
Dejan Milicic <dejan.milicic@gmail.com>
2022-10-17 11:53:15 +0200

    wip

--- Cuervo/Library.fs
+++ Cuervo/Library.fs
@@ -3,3 +3,23 @@
 module Say =
     let hello name =
         printfn "Hello %s" name
+
+    open Corax
+    open Voron.Util.Settings
+    open Voron
+
+
+    // 
+    //protected StorageEnvironmentOptions(
+    //  VoronPathSetting tempPath, 
+    //  IoChangesNotifications ioChangesNotifications, 
+    //  CatastrophicFailureNotification catastrophicFailureNotification)
+
+    // public VoronPathSetting(string path, string baseDataDir = null)
+    let tempPath = new VoronPathSetting(path = "", baseDataDir = "")
+    let options = StorageEnvironmentOptions.CreateMemoryOnly()
+    //let storageEnvOptions : StorageEnvironmentOptions = new StorageEnvironmentOptions
+    //let env : StorageEnvironment = new StorageEnvironment storageEnvOptions
+
+    //let searcher = new IndexSearcher(environment = env)
+    //var match1 = searcher.TermQuery("Id", "entry/1");
